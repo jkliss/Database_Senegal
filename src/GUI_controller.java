28f3a5fe3d08b6d6e2cbf374ae7d9fc98a1a1706
julius <julius.kliss@gmail.com>
2018-08-30 18:32:38 +0200

    update

--- src/GUI_controller.java
+++ src/GUI_controller.java
@@ -2,11 +2,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Window;
-import sun.awt.WindowIDProvider;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
+
 
 public class GUI_controller {
     @FXML
@@ -15,9 +14,29 @@ public class GUI_controller {
     @FXML
     private Label left_status;
 
+    @FXML
+    private DatePicker in_birthday;
+
+    @FXML
+    private DatePicker in_date;
+
+    @FXML
+    private TextField in_name;
+
+    @FXML
+    private TextField in_lastname;
+
+    @FXML
+    private TextField in_treatment;
+
+    @FXML
+    private TextField in_village;
+
+    @FXML
+    private TextField in_telephone;
 
     public void pushedLowerButton(javafx.event.ActionEvent actionEvent) {
         Window owner = wbutton.getScene().getWindow();
-        left_status.setText("SET NAME TO ");
+        left_status.setText("SET NAME TO " + in_name.getText());
     }
 }
